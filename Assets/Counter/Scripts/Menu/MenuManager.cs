1d4f2bafe50a2a2d225722887731efcb36005c61
Dzmitry Bahdanovich <d.bahdanovich@andersenlab.com>
2023-09-07 16:41:20 +0200

    Added menu scene

---
+++ Assets/Counter/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuManager : MonoBehaviour
+{
+    // we assign event onClick on Start button
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        //it is actually instructions for the compiler. (lines starts with # , such lines will not builded)
+        // inside unity it will compile EditorApplication.ExitPlaymode() but during buid Application.Quit()
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit(); // original code to quit Unity player
+        #endif
+    }
+
+    public void SettingsButton()
+    {
+        // SceneManager.LoadScene(1);
+    }
+}
